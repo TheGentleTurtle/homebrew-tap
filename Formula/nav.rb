ad37b204bd9fb4592f0025e7c56a4b2fdd5a60d8
TheGentleTurtle <TheGentleTurtle@users.noreply.github.com>
2026-04-23 18:31:02 -0400

    Bump nav to v0.11.1

--- Formula/nav.rb
+++ Formula/nav.rb
@@ -1,8 +1,8 @@
 class Nav < Formula
   desc "Tiny terminal file navigator with vim keys"
   homepage "https://github.com/TheGentleTurtle/nav"
-  url "https://github.com/TheGentleTurtle/nav/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "a7c45569e9b79de25fd5b6fd92602fa4e7795863195a673fe385b6b7638314b3"
+  url "https://github.com/TheGentleTurtle/nav/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "4e8e1f4eda87806f3ff5542d6fe128077f1fd8d95a5560e342d384409f817d5b"
   license "CC-BY-NC-4.0"
 
   depends_on "go" => :build
@@ -17,6 +17,10 @@ class Nav < Formula
 
         # --- nav - terminal directory navigator ---
         nav() {
+          if [ $# -gt 0 ]; then
+            command nav "$@"
+            return
+          fi
           local dir
           dir="$(NAV_WRAPPED=1 command nav)"
           if [ -n "$dir" ] && [ -d "$dir" ]; then
@@ -28,6 +32,6 @@ class Nav < Formula
   end
 
   test do
-    assert_match "nav", shell_output("#{bin}/nav --help 2>&1", 1)
+    assert_match "nav", shell_output("#{bin}/nav --help 2>&1")
   end
 end
