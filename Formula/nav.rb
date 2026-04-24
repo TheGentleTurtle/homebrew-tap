b8e9dbfc191bfcd8af5b30b647c6af3dad999b3d
TheGentleTurtle <TheGentleTurtle@users.noreply.github.com>
2026-04-23 20:14:58 -0400

    Bump nav to v0.14.0

--- Formula/nav.rb
+++ Formula/nav.rb
@@ -1,34 +1,14 @@
 class Nav < Formula
   desc "Tiny terminal file navigator with vim keys"
   homepage "https://github.com/TheGentleTurtle/nav"
-  url "https://github.com/TheGentleTurtle/nav/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "b08d738d308970a9e7091c0913687568f6d594825f4308502a50ec3ef7c56e7e"
+  url "https://github.com/TheGentleTurtle/nav/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "be6482847242e16c1a67cfbab9d7eb6bbd0b579336f874d1be80af2c4ed8bfba"
   license "CC-BY-NC-4.0"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
-  end
-
-  def caveats
-    <<~EOS
-      To use nav for changing directories, add this to your shell config:
-
-        # --- nav - terminal directory navigator ---
-        nav() {
-          if [ $# -gt 0 ]; then
-            command nav "$@"
-            return
-          fi
-          local dir
-          dir="$(NAV_WRAPPED=1 command nav)"
-          if [ -n "$dir" ] && [ -d "$dir" ]; then
-            cd "$dir"
-          fi
-        }
-        # --- end nav ---
-    EOS
   end
 
   test do
